54813199c550479632b6d13602375f230aab80d0
zameemallik <zameemallik@gmail.com>
2025-01-09 07:46:20 +0900

    post の title が 15文字以上の時に title を省略する処理をcssによる実装に変更。それに伴い不要となったtruncatedTitle関数の削除。

--- app/components/Thumbnail/Thumbnail.tsx
+++ app/components/Thumbnail/Thumbnail.tsx
@@ -8,11 +8,10 @@ type Props = {
 };
 
 const Thumbnail: React.FC<Props> = ({ post }) => {
-  const truncatedTitle = post.title.length > 15 ? post.title.slice(0, 15) + "..." : post.title;
   return (
     <Link href={`/posts/${post.id}`} key={post.id} className={styles.postItem}>
       <img src={post.imagePath} alt={post.title} width={250} height={150} />
-      <h2>{truncatedTitle}</h2>
+      <h2>{post.title}</h2>
     </Link>
   );
 };
