d37c40ff9887e4f8ed19c4d65ca4280d4a4cb0b9
goemon024 <hirotorics@gmail.com>
2025-01-13 17:38:47 +0900

    almost complete

--- app/components/Pagenation/Pagenation.tsx
+++ app/components/Pagenation/Pagenation.tsx
@@ -1,16 +1,15 @@
-import React,{ useState } from 'react'
-import style from './Pagenation.module.css'
-import { AiOutlineArrowRight,AiOutlineArrowLeft } from 'react-icons/ai';
-
+import React, { useState } from "react";
+import style from "./Pagenation.module.css";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 
 type PagenationProps = {
   postNumber: number; // postNumber の型を明示
 };
 
-const Pagenation: React.FC<PagenationProps> = ({postNumber}) => {
-  const [currentPage, setCurrentPage] = useState(1)
+const Pagenation: React.FC<PagenationProps> = ({ postNumber }) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const maxVisiblePages = 10; // 表示するページ番号の最大数
-  const totalPages = Math.ceil(postNumber/12);
+  const totalPages = Math.ceil(postNumber / 12);
   const startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
   const endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
 
@@ -18,56 +17,51 @@ const Pagenation: React.FC<PagenationProps> = ({postNumber}) => {
     if (page >= 1 && page <= totalPages) {
       // console.log("Page changed to:", page);
       setCurrentPage(page);
-
     }
   };
 
-
   return (
-    <div className={style.pagination}>
-    {/* 前へボタン */}
-    <button
-      className={style.previousButton}
-      onClick={() => handlePageChange(currentPage - 1)}
-      disabled={currentPage === 1}
-    >
-      <AiOutlineArrowLeft className={style.arrowIcon} />
-      <span className={style.spanStyle}></span>
-      Previous Page
-    </button>
+    <div className={style.pagenation}>
+      {/* 前へボタン */}
+      <button
+        className={style.previousButton}
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        <AiOutlineArrowLeft className={style.arrowIcon} />
+        <span className={style.spanStyle}></span>
+        Previous Page
+      </button>
 
-    {/* ページ番号ボタン */}
-    <div className={style.buttonsContainer}>
-    {Array.from({ length: endPage - startPage + 1 }, (_, index) => {
-      const page = startPage + index;
-      return (
-        <button
-          className={page===currentPage ? style.currentButton : style.pageButton}
-          key={page}
-          onClick={() => handlePageChange(page)}
-          disabled={currentPage === page}
-        >
-          {page}
-        </button>
-      );
-    })}
+      {/* ページ番号ボタン */}
+      <div className={style.buttonsContainer}>
+        {Array.from({ length: endPage - startPage + 1 }, (_, index) => {
+          const page = startPage + index;
+          return (
+            <button
+              className={page === currentPage ? style.currentButton : style.pageButton}
+              key={page}
+              onClick={() => handlePageChange(page)}
+              disabled={currentPage === page}
+            >
+              {page}
+            </button>
+          );
+        })}
+      </div>
+
+      {/* 次へボタン */}
+      <button
+        className={style.nextButton}
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next Page
+        <span className={style.spanStyle}></span>
+        <AiOutlineArrowRight className={style.arrowIcon} />
+      </button>
     </div>
+  );
+};
 
-    {/* 次へボタン */}
-    <button
-      className={style.nextButton}
-      onClick={() => handlePageChange(currentPage + 1)}
-      disabled={currentPage === totalPages}
-    >
-    Next Page
-    <span className={style.spanStyle}></span>
-    <AiOutlineArrowRight className={style.arrowIcon} />
-    </button>
-    </div>
-
-
-
-  )
-}
-
-export default Pagenation
+export default Pagenation;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-"use client"
-import React from 'react'
-import Pagenation from "./components/Pagenation/Pagenation"
+"use client";
+import React from "react";
+import Pagenation from "./components/Pagenation/Pagenation";
 
 import BlogHome from "./components/BlogHome";
 
 export default function Home() {
   return (
-  <div>
-  <BlogHome />;
-  {/* postNumberに任意の数字が入力されるようにする */}
-  <Pagenation postNumber={97}/>
-  </div>
-  )
+    <div>
+      <BlogHome />
+      <Pagenation postNumber={97} />;{/* postNumberに任意の数字が入力されるようにする */}
+    </div>
+  );
 }
