e6be2cdf0fe1e089669baa6b0669d1a6a6111ba0
goemon024 <hirotorics@gmail.com>
2025-01-13 17:29:19 +0900

    temporary commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+import React from 'react'
+import Pagenation from "./components/Pagenation/Pagenation"
+
 import BlogHome from "./components/BlogHome";
 
 export default function Home() {
-  return <BlogHome />;
+  return (
+  <div>
+  <BlogHome />;
+  {/* postNumberに任意の数字が入力されるようにする */}
+  <Pagenation postNumber={97}/>
+  </div>
+  )
 }
